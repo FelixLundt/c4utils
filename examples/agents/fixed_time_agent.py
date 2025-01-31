19d0727a08588aef705e662f9f136f4c05eeb69e
FelixLundt <felixlundt@gmail.com>
2025-01-31 10:45:30 +0100

    Slight change for more robustness.

--- examples/agents/fixed_time_agent.py
+++ examples/agents/fixed_time_agent.py
@@ -3,6 +3,7 @@ import numpy as np
 from c4utils.c4_types import Move, Player
 
 def generate_move(board: np.ndarray, player: Player, timeout: float) -> Move:
-    """Generate a random valid move."""
+    """Generate a dummy move (leftmost column)with a fixed time."""
     time.sleep(timeout/2)
-    return Move(0)
+    valid_moves = np.where(board[0, :] == 0)[0]
+    return Move(valid_moves[0])
