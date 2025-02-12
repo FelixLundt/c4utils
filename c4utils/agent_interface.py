eea957ba06319d3efa3ac71fcb21b3b6fcdd2ee8
FelixLundt <felixlundt@gmail.com>
2025-02-12 12:27:00 +0100

    Made agent validation timeout variable.

--- c4utils/agent_interface.py
+++ c4utils/agent_interface.py
@@ -2,19 +2,17 @@ import numpy as np
 from .c4_types import Player, AgentFunction, BOARD_SIZE, PLAYER1, PLAYER2, Move
 from .rules import is_valid_move
 
-VALIDATION_TIMEOUT = 1.0
-
-def validate_agent_function(func: AgentFunction) -> tuple[bool, None | Exception]:
+def validate_agent_function(func: AgentFunction, validation_timeout: float) -> tuple[bool, None | Exception]:
     """Validates that an agent function meets the required interface."""
     try:
-        valid = all([check_first_move(func), check_later_move(func)])
+        valid = all([check_first_move(func, validation_timeout), check_later_move(func, validation_timeout)])
         return valid, None
     except Exception as e:
         return False, e
 
 def check_first_move(func: AgentFunction) -> bool:
     board = np.zeros(BOARD_SIZE, dtype=Player)
-    move = func(board, PLAYER1, VALIDATION_TIMEOUT)
+    move = func(board, PLAYER1, validation_timeout)
     return is_valid_move(board, Move(move), PLAYER1)
 
 def check_later_move(func: AgentFunction) -> bool:
@@ -24,5 +22,5 @@ def check_later_move(func: AgentFunction) -> bool:
     board[0, 4] = PLAYER1
     board[1, 1] = PLAYER2
     board[1, 2] = PLAYER1
-    move = func(board, PLAYER2, VALIDATION_TIMEOUT)
+    move = func(board, PLAYER2, validation_timeout)
     return is_valid_move(board, Move(move), PLAYER2)
